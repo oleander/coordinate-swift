890b5fb9fe65c016e539585c2aeb12352bdb0b38
Linus Oleander <linus@oleander.nu>
2019-02-13 17:00:59 +0800

    Clean up

--- Package.swift
+++ Package.swift
@@ -12,9 +12,8 @@ let package = Package(
     ],
     dependencies: [
       .package(url: "https://github.com/oleander/distance-swift.git", .branch("master")),
-
       .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.2"),
-      .package(url: "https://github.com/Quick/Quick.git", from: "1.2.0"),
+      .package(url: "https://github.com/Quick/Quick.git", from: "1.2.0")
     ],
     targets: [
       .target(
